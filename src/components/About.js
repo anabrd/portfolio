511c3a2db6331729bd0a09ed129b3a2f9915e93a
anabrd <ana.d.brdar@gmail.com>
2021-04-14 18:59:54 +0200

    add heading animations

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,12 @@
 function About(props) {
 
     let data = props.data.filter(item => item.id == 1)[0];
-
     let tools = data.content.map(list => 
                                     <ul>
                                         {list.map(item => <li>{item}</li>)}
                                     </ul>);
     return (
-        <section id = {data.title}>
+        <section id={data.title}>
             <h2>{data.title}</h2>
             <p>{data.description}</p>
             <h3>{data.subheading}</h3>
