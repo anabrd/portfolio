1efeac06f66ec3c6d7c2b30aed7f36c9cc4bbe7f
anabrd <ana.d.brdar@gmail.com>
2021-04-18 16:56:52 +0200

    add contacts/projects/about  mobile/tablet query

--- src/components/About.js
+++ src/components/About.js
@@ -10,14 +10,17 @@ function About(props) {
     return (
         <section id={data.title.toLowerCase()}>
             <h2 className="section-heading">{data.heading}</h2>
-                    <p>{data.description}</p>
-                <div className="tools-wrapper">
-                    <h4>{data.subheading}</h4>
-                    <div className = "tools-list-wrapper">
-                        {tools}
-                    </div>
+            <div className="about-wrapper">
+
+            <p>{data.description}</p>
+            <div className="tools-wrapper">
+                <h4>{data.subheading}</h4>
+                <div className = "tools-list-wrapper">
+                    {tools}
                 </div>
-                <div className="circle-big"></div>
+            </div>
+            <div className="circle-big"></div>
+            </div>
         </section>
     )
 }
